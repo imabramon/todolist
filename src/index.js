3d3bb5ddf22006f0180388eb78fed83f393b4202
im_abramon <shelopugin.vlad@yandex.ru>
2024-02-20 14:36:29 +0300

    Keep calm and commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import App from './components/App'; 
+import App from './components/App'; ;
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root')) 
 
-root.render(<App />)
+root.render(<App />) 
