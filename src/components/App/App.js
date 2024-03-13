56cbfdcffed48d10bf8d5d633eebd3c36a85d4d3
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-13 12:13:43 +0300

    Переписал передаваемые функции в функциональном стиле

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable */
+import React, { useReducer, useState } from 'react'
 import './App.css'
 
 import TaskList from '../TaskList'
@@ -11,7 +12,7 @@ const changeById = (arr, id, callback) =>
     return [...acc, callback(curr)]
   }, [])
 
-class App extends React.Component {
+class App1 extends React.Component {
   maxID = 0
 
   constructor() {
@@ -131,6 +132,132 @@ class App extends React.Component {
       </section>
     )
   }
+}
+
+let maxID = 0 // мб сделать через замыкание
+
+const makeTask = (description, createdTime, time = 0, completed = false) => ({
+  id: maxID++,
+  completed,
+  description,
+  createdTime,
+  time,
+  isTimerRun: false,
+})
+
+const TaskActionTypes = {
+  delete: 'delete',
+  toggleCompeted: 'toggleCompeted',
+  updateTaskTime: 'updateTaskTime',
+  updateTaskTitle: 'updateTaskTitle',
+  add: 'add',
+}
+
+const addTask = (description, min, sec) => {
+  this.setState(({ items }) => ({
+    items: [...items, this.makeTask(description, new Date(), min * 60 + sec)],
+  }))
+}
+
+const toggleCompeted = (id) => {
+  this.setState(({ items }) => ({
+    items: items.map((el) => {
+      if (el.id !== id) return el
+
+      return {
+        ...el,
+        completed: !el.completed,
+      }
+    }),
+  }))
+}
+
+const deleteCompleted = () => {
+  this.setState(({ items }) => ({
+    items: items.filter(({ completed }) => !completed),
+  }))
+}
+
+const taskReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+/* 
+  deleteTaskHandler = id
+  toggleCompeletedHandler = id
+  updateTimeHandler = id newTime isTimerRun
+  updateTaskTitleHandler = id new Title
+  addTask = title min sec
+*/
+
+const ViewTypes = {
+  all: 'all',
+  completed: 'completed',
+  active: 'active',
+}
+
+const App = (props) => {
+  const [tasks, tasksDispathcer] = useReducer(taskReducer, [
+    makeTask('Make react app', new Date('2024-02-17T03:24:00'), 0, true),
+    makeTask('buy snus', new Date('2024-02-17T03:24:00')),
+    makeTask('go to shop', new Date('2024-02-17T03:24:00')),
+  ])
+
+  const addTask = (title, min, sec) => {
+    console.log(`add task with ${title} and time ${min}:${sec}`)
+  }
+
+  const deleteTask = (id) => {
+    console.log('delete task', id)
+  }
+
+  const toggleCompeted = (id) => {
+    console.log('toggle task', id)
+  }
+
+  const updateTaskTime = (id, newTime, isTimerRun) => {
+    console.log(`update task[${id} to new time ${newTime}. Timer is running: ${isTimerRun}`)
+  }
+
+  const updateTaskTitle = (id, newTitle) => {
+    console.log(`update task[${id} to new title ${newTitle}`)
+  }
+
+  const uncomletedCount = (() => 2)(tasks)
+
+  const deleteCompleted = () => {
+    console.log(`delete all completed`)
+  }
+
+  const [currentView, setCurrentView] = useState(ViewTypes.all)
+
+  const changeView = (newView) => {
+    console.log(`new view is ${newView}`)
+  }
+
+  return (
+    <section className="todoapp">
+      <NewTaskForm addTaskHandler={addTask} />
+      <section className="main">
+        <TaskList
+          items={tasks}
+          deleteTaskHandler={deleteTask}
+          toggleCompeletedHandler={toggleCompeted}
+          updateTimeHandler={updateTaskTime}
+          updateTaskTitleHandler={updateTaskTitle}
+        />
+        <Footer
+          uncomletedCount={uncomletedCount}
+          filterState={currentView}
+          deleteCompletedHandler={deleteCompleted}
+          changeFilterHandler={changeView}
+        />
+      </section>
+    </section>
+  )
 }
 
 export default App
