01b27a269eaacc11aa91633dae6a1cefd8759d06
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-13 20:47:51 +0300

    тудаааа

--- src/components/SearchInput/SearchInput.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './SearchInput.css'
-
-function SearchInput() {
-  return <input type="text" className="search-input" placeholder="search" />
-}
-
-export default SearchInput
